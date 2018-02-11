2a98fc471666f6c11439baf30f6a348252dc89aa
Anton <anton_iv_al@mail.ru>
2018-02-12 02:12:39 +0300

    rectangle for cut

---
+++ ImageCutter/ImageHelper.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ImageCutter {
+    public static class ImageHelper {
+        private static Bitmap _image;
+
+        public static Bitmap CutImage(Bitmap image) {
+            _image = image;
+
+            var rect = RectangleForCut();
+
+            var newImage = new Bitmap(2000, 2000);
+
+            var clone = _image.Clone(rect, _image.PixelFormat);
+            var graphics = Graphics.FromImage(newImage);
+            graphics.DrawImage(clone, new PointF(0, 0));
+
+            return newImage;
+        }
+
+        private static Rectangle RectangleForCut() {
+            int fromLeft = FirstPixelColumnFromLeft();
+
+            if(fromLeft < 0) return new Rectangle(0, 0, 1, 1);
+
+            int fromRight = FirstPixelColumnFromRight();
+            int fromUp = FirstPixelRowFromUp();
+            int fromDown = FirstPixelRowFromDown();
+
+            int x = fromLeft;
+            int y = fromUp;
+            int width = fromRight - fromLeft + 1;
+            int height = fromDown - fromUp + 1;
+
+            return new Rectangle(x, y, width, height);
+        }
+
+        private static int FirstPixelColumnFromLeft() {
+            for (int i = 0; i < _image.Width; i++) {
+                if (!IsPixelColumnEmpty(i)) return i;
+            }
+
+            return -1;
+        }
+
+        private static int FirstPixelColumnFromRight() {
+            for (int i = _image.Width - 1; i >= 0; i--) {
+                if (!IsPixelColumnEmpty(i)) return i;
+            }
+
+            return -1;
+        }
+
+        private static int FirstPixelRowFromUp() {
+            for (int i = 0; i < _image.Height; i++) {
+                if (!IsPixelRowEmpty(i)) return i;
+            }
+
+            return -1;
+        }
+
+        private static int FirstPixelRowFromDown() {
+            for (int i = _image.Height - 1; i >= 0; i--) {
+                if (!IsPixelRowEmpty(i)) return i;
+            }
+
+            return -1;
+        }
+
+        private static bool IsPixelColumnEmpty(int x) {
+            for (int i = 0; i < _image.Height; i++) {
+                if (!IsColorTransparent(_image.GetPixel(x, i))) return false;
+            }
+
+            return true;
+        }
+
+        private static bool IsPixelRowEmpty(int y) {
+            for (int i = 0; i < _image.Width; i++) {
+                if (!IsColorTransparent(_image.GetPixel(i, y))) return false;
+            }
+
+            return true;
+        }
+
+        private static bool IsColorTransparent(Color color) {
+            return color.A == 0;
+        }
+    }
+}
--- ImageCutter/Views/Main/MainPresenter.cs
+++ ImageCutter/Views/Main/MainPresenter.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows;
 
 namespace ImageCutter.Views.Main {
     public class MainPresenter : Presenter<MainWindow> {
         public MainPresenter(MainWindow view) : base(view) { }
 
         public void OnCutButtonClick() {
+            string dir = @"C:\Users\Anton\Downloads\tmp\";
+            string path = @"C:\Users\Anton\Downloads\tmp\test.png";
 
+            string newDir = dir + @"cutted\";
+            string newPath = newDir + "test_copy.png";
+            Directory.CreateDirectory(newDir);
+
+            var image = Image.FromFile(path) as Bitmap;
+
+            image = ImageHelper.CutImage(image);
+
+            image.Save(newPath, ImageFormat.Png);
+
+            image.Dispose();
         }
     }
 }
