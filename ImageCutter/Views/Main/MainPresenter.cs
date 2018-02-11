dabb77a0157913eaccd9d33a13042309ea245906
Anton <anton_iv_al@mail.ru>
2018-02-11 23:58:23 +0300

    interface

---
+++ ImageCutter/Views/Main/MainPresenter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageCutter.Views.Main {
+    public class MainPresenter : Presenter<MainWindow> {
+        public MainPresenter(MainWindow view) : base(view) { }
+
+        public void OnCutButtonClick() {
+
+        }
+    }
+}
---
+++ ImageCutter/Views/Main/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageCutter.Views.Main {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        private MainPresenter _presenter;
+
+        public MainWindow() {
+            InitializeComponent();
+            _presenter = new MainPresenter(this);
+        }
+
+        public string DirectoryText {
+            get => DirectoryControl.Text;
+            set => DirectoryControl.Text = value;
+        }
+
+        public int SizeText => int.Parse(SizeControl.Text);
+        public double BorderText => double.Parse(BorderControl.Text);
+
+        private void CutButtonControl_Click(object sender, RoutedEventArgs e) {
+            _presenter.OnCutButtonClick();
+        }
+    }
+}
